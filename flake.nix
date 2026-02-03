326a4150fa25c5ad0cf7c53fddfbf003c3d3829e
Rob Crowe <282571+rcrowe@users.noreply.github.com>
2026-02-03 19:12:37 +0000

    nix shell it

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs_24
+          pnpm
+        ]
+        ++ (if pkgs.stdenv.isDarwin then [ apple-sdk_15 ] else []);
+      };
+    });
+}
